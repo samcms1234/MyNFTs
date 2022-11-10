26fd73d0de970b6466d1a5fe2f58695ac099f94d
samcms1234 <samdevpro99@gmail.com>
2022-11-10 07:56:21 +0530

    added functionality to sell nft and upload metadata to ipfs

--- src/components/SellNFT.js
+++ src/components/SellNFT.js
@@ -11,6 +11,76 @@ export default function SellNFT () {
     const [message, updateMessage] = useState('');
     const location = useLocation();
 
+    async function OnChangeFile(e) {
+        var file = e.target.files[0];
+
+        try {
+            const response = await uploadFileToIPFS(file);
+            if(response.success == true) {
+                console.log("Upload image to Pinata", response.pinataURL)
+                setFileURL(response.pinataURL);
+            } 
+            }
+            
+            catch(e) {
+                console.log("Error during file upload", e)
+            }
+    }
+
+    async function uploadMetadataToIPFS() {
+        const {name, description, price} = formParams;
+
+        if(!name || !description || ~price || !fileURL)
+            return;
+
+        const nftJSON = {
+            name, description, price, image: fileURL
+        };
+
+        try {
+            const response = await uploadJSONToIPFS(nftJSON);
+            if(response.success === true) {
+                console.log("Upload JSON to Pinata: ", response);
+                return response.pinataURL;
+            }
+        } 
+        
+        catch(e) {
+            console.log("error uploading JSON metadata: ", e);
+        }
+
+    }
+
+    async function listNFT(e) {
+        e.preventDefault();
+
+        try {
+            const metadataURL = await uploadFileToIPFS();
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+
+            updateMessage("Please wait ... uploading (upto 5 mins)");
+
+            let contract = new ethers.Contract(Marketplace.address, Marketplace.abi, signer);
+
+            const price = ethers.utils.parseUnits(formParams.price, 'ether');
+            let listingPrice = await contract.getListPrice();
+            listingPrice = listingPrice.toString();
+
+            let transaction = await contract.createToken(metadataURL, price, {value: listingPrice});
+            await transaction.wait();
+
+            alert("Successully listed your NFT!");
+            updateMessage("");
+            updateFormParams({name:'', description:'', price:''});
+            window.location.replace("/");
+
+        } 
+        catch(e) {
+            alert("Upload error: "+e);
+        }
+    }
+
     return (
         <div className="">
         <Navbar></Navbar>
